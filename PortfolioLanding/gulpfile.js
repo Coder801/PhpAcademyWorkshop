785b35e798a49fa1303910b027416a2dac04c239
Dmitriy Onischenko <donischenko801@gmail.com>
2018-04-06 01:34:56 +0300

    Add example project

---
+++ PortfolioLanding/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp'); // Сборщик
+const sass = require('gulp-sass'); // Библиотека SASS
+const gulpImagemin = require('gulp-imagemin'); // Библиотека оптимизации изображений
+const browserSync = require('browser-sync').create(); // Библиотека LiveReload
+
+// Задача запуска сервера и отслеживание файлов
+// При каждом изменении в файлах будут запускатся соотвецтвующие задачи
+gulp.task('server', ['sass'], function() {
+    browserSync.init({
+        server: "."
+    });
+    gulp.watch("./src/scss/*.scss", ['sass']);
+    gulp.watch('./src/img/*', ['img']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Транскомпиляция SCSS в СSS
+gulp.task('sass', function() {
+  return gulp.src('./src/scss/*.scss') // Берем исходные файлы
+    .pipe(sass({includePaths: require('node-normalize-scss').includePaths})) // Транскомпиляция в CSS
+    .pipe(gulp.dest('./dist/css/')) // Складывает их в папку dist
+    .pipe(browserSync.stream()); // Перезагружаем браузер
+});
+
+// Минификация изображений
+gulp.task('img', function() {
+  return gulp.src('./src/img/*') // Берем исходные файлы
+    .pipe(gulpImagemin()) // Минификация изображений
+    .pipe(gulp.dest('./dist/img/')); // Складывает их в папку dist
+});
+
+// Gulp задача по умолчанию
+gulp.task('default', ['sass', 'img', 'server']);
